10952e4059c822e1d993be572e8ab20e98dc6478
nas7062 <nas7062@naver.com>
2025-12-13 11:04:33 +0900

    feat : follower,following 목록에서 해당 유저의 psot로 이동

--- src/app/(narrow)/[name]/_components/FollowUser.tsx
+++ src/app/(narrow)/[name]/_components/FollowUser.tsx
@@ -1,6 +1,7 @@
 import { FollowButton } from "@/src/app/_components/FollowButton";
 import { IUser } from "@/src/app/_components/PostDetail";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function FollowUser({
   follow,
@@ -12,20 +13,24 @@ export default function FollowUser({
   return (
     <div key={follow.id}>
       <div className="flex gap-4 items-center">
-        <Image
-          src={follow?.image ? follow.image : "/hello.png"}
-          alt="유저 이미지"
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-        <div className="flex flex-col gap-1">
-          <div>{follow.name}</div>
-          <div>{follow.descript}</div>
-        </div>
-        <div className="ml-auto">
-          <FollowButton userId={user?.id} targetId={follow.id} />
-        </div>
+        <Link href={`/${follow.id}/posts`} className="flex gap-4">
+          <Image
+            src={follow?.image ? follow.image : "/hello.png"}
+            alt="유저 이미지"
+            width={50}
+            height={40}
+            className="rounded-full"
+          />
+          <div className="flex flex-col gap-1">
+            <div>{follow.name}</div>
+            <div>{follow.descript}</div>
+          </div>
+        </Link>
+        {user && (
+          <div className="ml-auto">
+            <FollowButton userId={user?.id} targetId={follow.id} />
+          </div>
+        )}
       </div>
     </div>
   );
