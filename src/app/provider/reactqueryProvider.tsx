392dd79da66bf2daf0dbfbd09c6c264d1d96d43f
nas7062 <nas7062@naver.com>
2025-12-26 14:05:00 +0900

    refactor : tanstackDevtools 개발 모드에서만 렌더링

--- src/app/provider/reactqueryProvider.tsx
+++ src/app/provider/reactqueryProvider.tsx
@@ -14,7 +14,9 @@ export default function ReactQueryProvider({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 }
