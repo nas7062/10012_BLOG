180be45a373a161602a85c671399e8122bcd30a2
nas7062 <nas7062@naver.com>
2025-12-26 18:04:29 +0900

    style : post Layout변경

--- src/app/_components/Post.tsx
+++ src/app/_components/Post.tsx
@@ -35,7 +35,7 @@ export default function Post({
   if (!writeUser || isError) return;
   return (
     <div
-      className="flex flex-col max-w-[300px] shadow-xl gap-2 pb-4 rounded-md
+      className="flex flex-col  w-[300px] h-[420px] shadow-xl gap-2 pb-4 rounded-md
                 transition-transform duration-350
                 hover:-translate-y-2 hover:shadow-2xl
                 cursor-pointer relative"
