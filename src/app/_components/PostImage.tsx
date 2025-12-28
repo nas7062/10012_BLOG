79cd6a313aae2f9465b8ccf4c36bb417d1eb2169
nas7062 <nas7062@naver.com>
2025-12-28 15:12:34 +0900

    fix : prioity 범위 변경

--- src/app/_components/PostImage.tsx
+++ src/app/_components/PostImage.tsx
@@ -21,7 +21,7 @@ export default function PostImage({ src, alt, priority }: Props) {
       loading={priority ? "eager" : "lazy"}
       className="rounded-md h-60 aspect-square object-cover w-full"
       quality={70}
-      placeholder="blur"
+      placeholder={priority ? "empty" : "blur"}
       blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGBkRMU/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAZEQADAAMAAAAAAAAAAAAAAAAAAQIDESH/2gAMAwEAAhEDEQA/AD1wu5lKrWwKCiRdkJZPFSCHONVWNKLB7BGkqYSNMG5I2MkWk3bBtShIk2N2Mc8xY2Mk2NR6Q2NgqNPYq7h4EGxUamxkpNL1N0W8Xk/"
     />
   );
