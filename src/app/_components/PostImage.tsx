e62a7627726bfc24420f149ba61c5b50e4ddac3f
nas7062 <nas7062@naver.com>
2025-12-28 14:53:55 +0900

    refactor : postImage 조건 변경

--- src/app/_components/PostImage.tsx
+++ src/app/_components/PostImage.tsx
@@ -11,14 +11,16 @@ export default function PostImage({ src, alt, priority }: Props) {
     <Image
       src={src}
       alt={alt}
-      width={300}
-      height={300}
-      sizes="(max-width: 450px) 100vw, (max-width: 768px) 50vw, (max-width: 1280px) 300px, 300px"
+      width={240}
+      height={240}
+      sizes="(max-width: 640px) 90vw,
+       (max-width: 1024px) 45vw,
+       240px"
       priority={priority}
       fetchPriority={priority ? "high" : "auto"}
       loading={priority ? "eager" : "lazy"}
-      className="rounded-md max-h-60 aspect-square object-cover w-full"
-      quality={85}
+      className="rounded-md h-60 aspect-square object-cover w-full"
+      quality={70}
       placeholder="blur"
       blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGBkRMU/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAZEQADAAMAAAAAAAAAAAAAAAAAAQIDESH/2gAMAwEAAhEDEQA/AD1wu5lKrWwKCiRdkJZPFSCHONVWNKLB7BGkqYSNMG5I2MkWk3bBtShIk2N2Mc8xY2Mk2NR6Q2NgqNPYq7h4EGxUamxkpNL1N0W8Xk/"
     />
