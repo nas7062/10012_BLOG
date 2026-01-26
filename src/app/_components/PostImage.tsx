c9f9664493796835a41827fe2dc577554787945e
nas7062 <nas7062@naver.com>
2026-01-26 16:45:58 +0900

    fix : 이미지 최적화 조건 변경

--- src/app/_components/PostImage.tsx
+++ src/app/_components/PostImage.tsx
@@ -13,12 +13,10 @@ export default function PostImage({ src, alt, priority }: Props) {
     alt={alt}
     width={300}
     height={240}
-    sizes="(max-width: 450px) 100vw, (max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, 20vw"
-    priority={priority}
+     sizes="240px"
+    priority={priority ?? false}
     className="rounded-md h-60 aspect-square object-cover w-full"
     quality={75}
-    placeholder="blur"
-    blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
   />
   );
 }
