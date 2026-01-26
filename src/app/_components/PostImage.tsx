10e1ba8aad676a98113023bfa55b8341841480b7
nas7062 <nas7062@naver.com>
2026-01-26 16:51:14 +0900

    fix : 이미지 최적화 조건 변경

--- src/app/_components/PostImage.tsx
+++ src/app/_components/PostImage.tsx
@@ -13,8 +13,9 @@ export default function PostImage({ src, alt, priority }: Props) {
     alt={alt}
     width={300}
     height={240}
-     sizes="240px"
-    priority={priority ?? false}
+    sizes="(max-width: 450px) 100vw, (max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, 20vw"
+    priority={priority === true}
+    loading={priority ? "eager" : "lazy"}
     className="rounded-md h-60 aspect-square object-cover w-full"
     quality={75}
   />
